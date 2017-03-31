a97d2fd3d3c3b0d45ec36e01f5e4a8e88371e66e
Kennon <kennondmckeever@email.arizona.edu>
2017-03-31 04:21:17 -0700

    Updated the Dispatcher Node. It doesn't quite work yet though.

--- src/DispatcherNode.cpp
+++ src/DispatcherNode.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/highgui/highgui.hpp> //FIXME Not exactly sure why this one is here; maybe it's for displaying the images that come in?
 #include <cv_bridge/cv_bridge.h> //The openCV bridge that image_transport needs
 #include <ros/console.h> //This is for ROS_ERROR and stuff.
+#include <boost/filesystem.hpp> //For creating directtories if needed
 
 int main(int argc, char** argv)
 {
@@ -14,21 +15,51 @@ int main(int argc, char** argv)
 //	cv::waitKey(30); //No idea what this line does...
 //	cv::imshow("view", image); //This can be used to see the image as it's published
 
-	ros::Rate loop_rate(5); //rate is the number of loop iterations per second
+	//This block makes sure that the needed directories are set up (I'm pretty sure they should be, since this node might end up running from one of them).
+	boost::filesystem::create_directories("~/images/new");
+	boost::filesystem::create_directories("~/images/processed");
+	boost::filesystem::create_directories("~/images/unsuccessful");
+	//NOTE:: IF THERE IS AN ISSUE WITH PERMISSIONS FOR SOME REASON, IT MIGHT BE THAT THE LINES OF CODE ABOVE ARE REMOVING EXECUTE PERMISSIONS. JUST SOMETHING TO CONSIDER
+
+	boost::filesystem::path im_fetch_path("~/images/new"); //Will be used to get images
+	if (!boost::filesystem::exists(im_fetch_path))
+	{
+		ROS_ERROR("Oh no. ~/images/new isn't a directory =(");
+		return(-1);
+	}
+	else if (boost::filesystem::is_regular_file(im_fetch_path))
+	{
+		ROS_ERROR("Oh no. ~/images/new is a regular file =(");
+		return(-1);
+	}
+
+	ros::Rate loop_rate(25); //rate is the number of outer loop iterations per second
+	//Also, we need the loop rate to be relatively quick becuase we don't want delay in dispatching images. Delay in dispatching images could lead to wasted execution time and potential incorrect location stamps.
+
 	while(n.ok())
 	{
-		//The line below is the one tha actually retrieves the image. It should probably be changed to get the image from CHDK or GPhoto2.
-		cv::Mat newImage = cv::imread("/home/kennon/images/BrentRambo.jpg", CV_LOAD_IMAGE_COLOR);
+		//We need to dispatch each of the images in the new directory
+		for( boost::filesystem::directory_entry& current_path : boost::filesystem::directory_iterator(im_fetch_path))
+		{
+			if (boost::filesystem::is_directory(current_path)
+			{
+				//ROS_INFO("There's a directory in the ~/images/new path; this is currently unhandled.);
+			}
+			else
+			{
+				cv::Mat newImage = cv::imread(current_path.filename(), CV_LOAD_IMAGE_COLOR);
 
-		if(newImage.data == NULL)//The image was read improperly if this fails...
-		{
-			ROS_ERROR("I was unable to read the file image file.");
-		}
-		else
-		{
-			sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", newImage).toImageMsg();
-			im_pub.publish(msg);
-		}
+				if(newImage.data == NULL)//The image was read improperly if this fails...
+				{
+					ROS_ERROR("I was unable to read the file image file.");
+				}
+				else
+				{
+					sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", newImage).toImageMsg();
+					im_pub.publish(msg);
+				}
+			}
+		} //Ends the for loop
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
