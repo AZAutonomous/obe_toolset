e2ce5fc691db08baadb7e98cb9f4f27d70749c47
Kennon McKeever <kennondmckeever@email.arizona.edu>
2017-05-24 13:28:09 -0800

    updated odroid_env_loader to source devel/setup.bash

--- scripts/odroid_env_loader.sh
+++ scripts/odroid_env_loader.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . /opt/ros/indigo/setup.sh #This line is from the machine tag page
+. /home/odroid/catkin_ws/devel/setup.bash #Kennon added this to help it find the Processor and Dispatcher nodes
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/odroid/catkin_ws/src/obe_toolset #recommended in an answer found on rosAnswers
 export ROS_MASTER_URI=http://192.168.1.5:11311 #Kennon added this
 export ROS_IP=`hostname -I | tr " " "\n" | grep 192` #Kennon added this (chooses the 192.* address (in case there are multiple))
