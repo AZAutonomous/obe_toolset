8e97f26bc47c770a9270e12203e4fa6d4154eae0
Kennon <kennondmckeever@email.arizona.edu>
2017-05-23 17:42:42 -0700

    Updated a few things, but I don't know how to find geographiclib

--- src/DispatcherNode.cpp
+++ src/DispatcherNode.cpp
@@ -1,5 +1,4 @@
 #include <ros/ros.h> //For ROS itself
-//#include <image_transport/image_transport.h> //For the image transport class. Still needs the vc bridge though.
 #include <opencv2/highgui/highgui.hpp> //FIXME Not exactly sure why this one is here; maybe it's for displaying the images that come in?
 #include <cv_bridge/cv_bridge.h> //The openCV bridge that image_transport needs
 #include <ros/console.h> //This is for ROS_ERROR and stuff.
@@ -7,10 +6,42 @@
 #include <obe_toolset/ImageAndPose.h> //Needed for the custom Image and Pose message type
 #include <string>
 #include <vector>
+#include <GeographicLib/UTMUPS.hpp> //for conversion from Lat Lon to UTM
 
 //#include "std_msgs/String.h"
 //#define ONEATATIME //if this is defined, it will do one file at a time and pause in between files.
 
+//Global variables and classes that need to be accessed in the callback =(
+double utm_x, utm_x_prev;
+double utm_y, utm_y_prev;
+double utm_z, utm_z_prev;
+ros::Time timestamp, timestamp_prev;
+static const double SEARCH_AREA_ALTITUDE = 0; //This probably needs to be updated for camera agl altitude location calculations to be correct in the Processor nodes
+int fix_wait_ctr = -2;
+
+void locationCallback(const sensor_msgs::NavSatFix msg) //all this does is update the global variables listed above (for location).
+{
+	if (msg.status == -1)
+	{
+		return; //we don't have a fix and can't update anything =/
+	}
+	else
+	{
+		if (fix_wait_ctr < 0)
+			fix_wait_ctr += 1;
+		//move the current data to the old data slot
+		utm_x_prev = utm_x;
+		utm_y_prev = utm_y;
+		utm_z_prev = utm_z;
+		timestamp_prev = timestamp;
+
+		int zone; //dummy var for the UTMUPS conversion. This might be important at some point though, so maybe we should send it along with our ImAndPose stuff...
+		bool northp; //Hemisphere (true==northern)
+		GeographicLib::UTMUPS::Forward(msg.latitude, msg.longitude, &zone, &northp, &utm_x, &utm_y);
+		utm_z = msg.altitude;
+		timestamp = msg.header.stamp
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -22,6 +53,7 @@ int main(int argc, char** argv)
 		ROS_INFO("the parameter 'numPipes' for the dispatcher node hasn't been specified; assuming 1 to ensure that no images are lost. This may cause severe back-up issues in a long mission.");
 	n.param<int>("numPipes", numOfPipes, 1); //gets the numPipes param (so that we know where to publish), defaulting to 1 if it can't be read.
 
+	n.subscribe("/mavros/global_position/global", 0, locationCallack);
 	std::vector<ros::Publisher> impose_pub_vector(numOfPipes); //vector of publishers
 	for(int i = 1; i <= numOfPipes; ++i)
 	{
@@ -41,6 +73,7 @@ int main(int argc, char** argv)
 	//This block makes sure that the needed directories are set up (I'm pretty sure they should be, since this node might end up running from one of them).
 	namespace fs = boost::filesystem;
 
+		//NOTE: Not sure how relative paths will be handled when a launchfile runs this remotely...
 	fs::path new_path("../images/new/"); //Will be used to get images.
 	fs::path processed_path("../images/processed/");
 	fs::path roi_path("../images/rois/");
@@ -76,61 +109,66 @@ int main(int argc, char** argv)
 
 	while(n.ok())
 	{
-		//We need to dispatch each of the images in the new directory
-		ROS_INFO("Looking at path %s", im_fetch_path.string().c_str());
-		fs::directory_iterator end_itr;
-		for(fs::directory_iterator cur_path_itr(im_fetch_path); cur_path_itr != end_itr; cur_path_itr++)
+		//We need to first make sure that the GPS Fix is ready (should take 1 second if the GPS is already "warmed up"
+		if(fix_wait_ctr)
 		{
-			ROS_INFO("I'm looking at this file: %s", cur_path_itr->path().string().c_str());
-			if(fs::is_directory(cur_path_itr->path()))
+			ROS_INFO_THROTTLE(5,"Waiting for GPS Fix"); //Display at most once every 5 seconds
+		}
+		else
+		{
+			//We need to dispatch each of the images in the new directory
+			fs::directory_iterator end_itr;
+			for(fs::directory_iterator cur_path_itr(im_fetch_path); cur_path_itr != end_itr; cur_path_itr++)
 			{
-				ROS_INFO_STREAM_ONCE("There's a directory in the ~/images/new path; this is currently unhandled.");
-				fs::rename(cur_path_itr->path(), error_path / cur_path_itr->path().filename());
-
-				//FIXME Don't forget to move this file to the folder for files with errors.
-
-			}
-			else
-			{
-				cv::Mat newImage = cv::imread(cur_path_itr->path().string(), CV_LOAD_IMAGE_COLOR);
-
-				if(newImage.data == NULL)//The image was read improperly if this fails...
+				ROS_INFO("I'm looking at this file: %s", cur_path_itr->path().string().c_str());
+				if(fs::is_directory(cur_path_itr->path()))
 				{
-					ROS_ERROR("I was unable to read the file image file.");
+					ROS_INFO_STREAM_ONCE("There's a directory in the ~/images/new path; it is being moved to the error folder.");
 					fs::rename(cur_path_itr->path(), error_path / cur_path_itr->path().filename());
+					//FIXME Don't forget to move this file to the folder for files with errors.
+
 				}
 				else
 				{
-					obe_toolset::ImageAndPose impose_msg; //This is the custom message type that has a sensor_msgs::Image msg and a std_msgs::Pose message.
-					impose_msg.image = *(cv_bridge::CvImage(std_msgs::Header(), "bgr8", newImage).toImageMsg()); //The meat of this line is from the image_transport tutorial; I just de-reference their piece to get a sensor_msgs::Image.  Note: There's probably a better way to do this, but it will work for now.
+					cv::Mat newImage = cv::imread(cur_path_itr->path().string(), CV_LOAD_IMAGE_COLOR);
 
-					//This is the point where the fakeing things comes in.
-					impose_msg.x = 0; //fakes an x UTM value
-					impose_msg.y = 0; //etc...
-					impose_msg.z = 60.96; //This is actually a decent guess (~200 ft)
-					impose_msg.roll = 0.0;
-					impose_msg.pitch = 0.0;
-					impose_msg.yaw = 0.0; //this is the only one that's used as of 4.26.17
-					//End the faking it stuff.
+					if(newImage.data == NULL)//The image was read improperly if this fails...
+					{
+						ROS_ERROR("I was unable to read the image file.");
+						fs::rename(cur_path_itr->path(), error_path / cur_path_itr->path().filename());
+					}
+					else
+					{
+						obe_toolset::ImageAndPose impose_msg; //This is the custom message type that has a sensor_msgs::Image msg and a std_msgs::Pose message.
+						impose_msg.image = *(cv_bridge::CvImage(std_msgs::Header(), "bgr8", newImage).toImageMsg()); //The meat of this line is from the image_transport tutorial; I just de-reference their piece to get a sensor_msgs::Image.  Note: There's probably a better way to do this, but it will work for now.
 
-					//publish to the current pipe that's due for another message. NOTE: In the future, this could have a system that keeps track of busy nodes so that no particular node gets bogged down. I'm kind of assuming that we have enough nodes and a fast enough ROI algorithm and randomness is on our side so that this doesn't get out of hand.
-					impose_pub_vector[currentPipeMinusOne].publish(impose_msg); //send the impose message.
+						//This is the point where the fakeing things comes in.
+						impose_msg.x = 0; //fakes an x UTM value
+						impose_msg.y = 0; //etc...
+						impose_msg.z = 60.96; //This is actually a decent guess (~200 ft)
+						impose_msg.roll = 0.0;
+						impose_msg.pitch = 0.0;
+						impose_msg.yaw = 0.0; //this is the only one that's used as of 4.26.17
+						//End the faking it stuff.
 
-					//set up the current pipe for the next time we publish something.
-					currentPipeMinusOne++;
-					currentPipeMinusOne %= numOfPipes; //we want to wrap around
+						//publish to the current pipe that's due for another message. NOTE: In the future, this could have a system that keeps track of busy nodes so that no particular node gets bogged down. I'm kind of assuming that we have enough nodes and a fast enough ROI algorithm and randomness is on our side so that this doesn't get out of hand.
+						impose_pub_vector[currentPipeMinusOne].publish(impose_msg); //send the impose message.
 
-					//Now that we've published it, we can move the file to the processed folder
-					fs::rename(cur_path_itr->path(), processed_path / cur_path_itr->path().filename());
-					#ifdef ONEATATIME
-					break;
-					#endif
+						//set up the current pipe for the next time we publish something.
+						currentPipeMinusOne++;
+						currentPipeMinusOne %= numOfPipes; //we want to wrap around
+
+						//Now that we've published it, we can move the file to the processed folder
+						fs::rename(cur_path_itr->path(), processed_path / cur_path_itr->path().filename());
+						#ifdef ONEATATIME
+						break;
+						#endif
+					}
 				}
-			}
-		} //Ends the for loop
+			} //Ends the for loop
+		}//ends the if statement that waits for a GPS Fix
 		ros::spinOnce();
 		loop_rate.sleep();
-		ROS_INFO("I'm done sleeping");
 	}
 	//It shouldn't get here until you hit ctrl-C, but we still need to specify a return value:
 	return 0;
