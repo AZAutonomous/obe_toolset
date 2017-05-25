e8f6879e0c9fb27516d72dacdcc787ac1ce3c3f5
Kennon McKeever <kennondmckeever@email.arizona.edu>
2017-05-25 00:09:46 -0800

    Added the hashbang thing to the scripts

--- scripts/capture.sh
+++ scripts/capture.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /home/odroid/images/new
 sh ~/develop/chdkptp/chdkptp.sh -c -e"rec" #Place the camera into record mode
 sh ~/develop/chdkptp/chdkptp.sh -c -e"rs -shots=8000 -quick -sv=80 -av=2.8 -tv=1/2000" #take a bunch of shots and download them as we go
